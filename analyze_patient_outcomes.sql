63ca7ec52f135786f014b399f3b86791e520177e
ZainaManzoor25 <zainamanzoor58@gmail.com>
2025-08-26 16:22:44 -0500

    Update analyze_patient_outcomes.sql

--- analyze_patient_outcomes.sql
+++ analyze_patient_outcomes.sql
@@ -1,5 +1,5 @@
 -- analyze_patient_outcomes.sql
 SELECT diagnosis, COUNT(*) AS num_visits, ROUND(AVG(treatment_cost),2) AS avg_cost
-FROM PATIENT_VISITS_NOCLUSTER
+FROM HEALTH_DB.LABS.PATIENT_VISITS_NOCLUSTER
 GROUP BY diagnosis
 ORDER BY num_visits DESC;
